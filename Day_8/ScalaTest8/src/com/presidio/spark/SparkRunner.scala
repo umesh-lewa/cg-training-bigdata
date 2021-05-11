718d6651a35483945498506aab27495de2a812ca
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-05-11 08:58:12 +0530

    change pom proj name

--- Day_8/ScalaTest8/src/com/presidio/spark/SparkRunner.scala
+++ Day_8/ScalaTest8/src/com/presidio/spark/SparkRunner.scala
@@ -6,12 +6,22 @@ object SparkRunner {
 
   def main(args: Array[String]): Unit = {
 
+    /*
     val sparkConf = new SparkConf()
     sparkConf.setAppName("CodaDataJOB")
     sparkConf.setMaster("spark://Umeshs-MacBook-Pro.local:7077")
     val sparkContext = new SparkContext(sparkConf)
    //val textFileRDD = sparkContext.textFile("/Users/umeshmoorthy/scala/testData/auth.csv")
     val textFileRDD = sparkContext.textFile("hdfs://localhost:9000/testData/auth.csv")
+    */
+
+    // for docker jar
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("CodaDataJOB")
+    sparkConf.setMaster("spark://eb0988bbd19c:7077")
+    val sparkContext = new SparkContext(sparkConf)
+    //val textFileRDD = sparkContext.textFile("/Users/umeshmoorthy/scala/testData/auth.csv")
+    val textFileRDD = sparkContext.textFile("/usr/bin/bd/fs/data/auth.csv")
 
     val mappedRDD = textFileRDD.map(each => {
       val columns = each.split(",")
